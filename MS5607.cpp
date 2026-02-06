5c5b646754d004de61b8dcd51c5a68daecab6291
SleepyWoodpecker <shawnweichew@gmail.com>
2026-02-05 21:43:51 -0800

    chore: change init method name

--- MS5607.cpp
+++ MS5607.cpp
@@ -5,20 +5,21 @@
 
 // public methods
 MS5607::MS5607(SPIClass *spi_bus, uint8_t cs_pin, OSR_t osr_rate)
-    :_spi(spi_bus), _CS_pin(cs_pin), _spi_settings(10000000, SPI_MSBFIRST, SPI_MODE0),
-    _last_calculated_temperature(-1), _dT(-1), _last_calculated_actual_sensitivity(-1), _last_calculated_offset(-1)
-{  
+    : _spi(spi_bus), _CS_pin(cs_pin), _spi_settings(10000000, SPI_MSBFIRST, SPI_MODE0),
+      _last_calculated_temperature(-1), _dT(-1), _last_calculated_actual_sensitivity(-1),
+      _last_calculated_offset(-1) {
     pinMode(_CS_pin, OUTPUT);
     digitalWrite(_CS_pin, HIGH);
     set_osr_rate(osr_rate);
 }
 
-bool MS5607::initialize() 
-{
-    if (!_test_spi()) return false;
+bool MS5607::begin() {
+    if (!_test_spi())
+        return false;
 
     _send_command(MS5607_CMD_RESET);
-    delay(3); // ref: page 10 of datasheet says to wait 2.8ms after sending reset sequence
+    delay(3); // ref: page 10 of datasheet says to wait 2.8ms after sending reset
+              // sequence
 
     _read_calibration_coefficients();
 
@@ -26,50 +27,49 @@ bool MS5607::initialize()
     return _c1 >= 0 && _c2 >= 0 && _c3 >= 0 && _c4 >= 0 && _c5 >= 0 && _c6 >= 0
 }
 
-void MS5607::set_osr_rate(OSR_t osr_rate) 
-{
-    switch(osr_rate) {
-        case OSR256:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR256;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR256;
-            _adc_conversion_time_micro = 600;
-            break;
-        case OSR512:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR512;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR512;
-            _adc_conversion_time_micro = 1200;
-            break;
-        case OSR1024:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR1024;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR1024;
-            _adc_conversion_time_micro = 2300;
-            break;
-        case OSR2048:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR2048;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR2048;
-            _adc_conversion_time_micro = 4600;
-            break;
-        case OSR4096:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR4096;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR4096;
-            _adc_conversion_time_micro = 9100;
-            break;
-        default:
-            _pressure_command = MS5607_CMD_CONVERT_D1_OSR256;
-            _temperature_command = MS5607_CMD_CONVERT_D2_OSR256;
-            _adc_conversion_time_micro = 600;
-            break;
+void MS5607::set_osr_rate(OSR_t osr_rate) {
+    switch (osr_rate) {
+    case OSR256:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR256;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR256;
+        _adc_conversion_time_micro = 600;
+        break;
+    case OSR512:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR512;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR512;
+        _adc_conversion_time_micro = 1200;
+        break;
+    case OSR1024:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR1024;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR1024;
+        _adc_conversion_time_micro = 2300;
+        break;
+    case OSR2048:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR2048;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR2048;
+        _adc_conversion_time_micro = 4600;
+        break;
+    case OSR4096:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR4096;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR4096;
+        _adc_conversion_time_micro = 9100;
+        break;
+    default:
+        _pressure_command = MS5607_CMD_CONVERT_D1_OSR256;
+        _temperature_command = MS5607_CMD_CONVERT_D2_OSR256;
+        _adc_conversion_time_micro = 600;
+        break;
     }
 }
 
 /**
  * Get the uncompensated temperature reading
- * 
- * @param reading_is_valid: flag to check if the returned reading is valid. ignore the reading if the reading is invalid
+ *
+ * @param reading_is_valid: flag to check if the returned reading is valid.
+ * ignore the reading if the reading is invalid
  */
-uint32_t MS5607::read_raw_temperature(bool &reading_is_valid) 
-{
-   _send_command(_temperature_command);
+uint32_t MS5607::read_raw_temperature(bool &reading_is_valid) {
+    _send_command(_temperature_command);
 
     delayMicroseconds(_adc_conversion_time_micro);
 
@@ -83,11 +83,10 @@ uint32_t MS5607::read_raw_temperature(bool &reading_is_valid)
 /**
  * Calculate the actual temperature reading
  * ref: page 8 of datasheet
- * 
+ *
  * @return: temp with 0.01 C resolution
  */
-int32_t MS5607::calculate_temperature(uint32_t raw_temperature)
-{
+int32_t MS5607::calculate_temperature(uint32_t raw_temperature) {
     // calculate preliminary values first, if invalid, discard these values
     float dT = raw_temperature - _c5;
     float calculated_temp = 2000 + dT * _c6;
@@ -96,7 +95,7 @@ int32_t MS5607::calculate_temperature(uint32_t raw_temperature)
     if (!(-4000 <= calculated_temp && calculated_temp <= 8500)) {
         return -1;
     }
-    
+
     _dT = dT;
     _last_calculated_temperature = calculated_temp;
 
@@ -108,13 +107,13 @@ int32_t MS5607::calculate_temperature(uint32_t raw_temperature)
 
 /**
  * Get the uncompensated pressure reading
- * 
- * @param reading_is_valid: flag to check if the returned reading is valid. ignore the reading if the reading is invalid
+ *
+ * @param reading_is_valid: flag to check if the returned reading is valid.
+ * ignore the reading if the reading is invalid
  */
-uint32_t MS5607::read_raw_pressure(bool &reading_is_valid) 
-{
+uint32_t MS5607::read_raw_pressure(bool &reading_is_valid) {
     _send_command(_pressure_command);
-    
+
     delayMicroseconds(_adc_conversion_time_micro);
 
     uint32_t raw_result = _read_adc();
@@ -127,16 +126,18 @@ uint32_t MS5607::read_raw_pressure(bool &reading_is_valid)
 /**
  * Calculate the actual pressure reading
  * ref: page 8 of datasheet
- * 
+ *
  * @return: pressure with 0.01 mbar resolution
  */
-int32_t MS5607::calculate_pressure(uint32_t raw_pressure)
-{
+int32_t MS5607::calculate_pressure(uint32_t raw_pressure) {
     if (_last_calculated_offset == -1 || _last_calculated_actual_sensitivity == -1) {
         return -1;
     }
 
-    float calculated_pressure = (raw_pressure * _last_calculated_actual_sensitivity * 4.768317582E-7 - _last_calculated_offset) / (1 << 15);
+    float calculated_pressure =
+        (raw_pressure * _last_calculated_actual_sensitivity * 4.768317582E-7 -
+         _last_calculated_offset) /
+        (1 << 15);
     // check that the calculated pressure is within range
     if (!(1000 <= calculated_pressure && calculated_pressure <= 120000)) {
         return -1;
@@ -147,45 +148,45 @@ int32_t MS5607::calculate_pressure(uint32_t raw_pressure)
 
 /**
  * Calculate the altitude based on current temperature and pressure readings
- * 
+ *
  * @param t current temperature reading
  * @param p current pressure reading
  * @return altitude with 5 m resolution(?)
  */
-float MS5607::get_altitude(uint32_t t, uint32_t p){
-    return (153.84615*(pow(p,0.19) - 1)*(t+273.15)) / 1e3;
+float MS5607::get_altitude(uint32_t t, uint32_t p) {
+    return (153.84615 * (pow(p, 0.19) - 1) * (t + 273.15)) / 1e3;
 }
 
 void MS5607::dump_calibration_coeffs() {
-    Serial.printf("c1: %.5f, c2: %.5f, c3: %.5f, c4: %.5f, c5: %.5f, c6: %.5f\n", _c1, _c2, _c3, _c4, _c5, _c6);
+    Serial.printf(
+        "c1: %.5f, c2: %.5f, c3: %.5f, c4: %.5f, c5: %.5f, c6: %.5f\n", _c1, _c2, _c3, _c4, _c5, _c6
+    );
 };
-
 
 // private methods
 
 /**
- * Reload the calibration values for the sensor class. Set up the coefficients with their right scaling factor as well
- * ref: page 11 and 12 of datasheet
+ * Reload the calibration values for the sensor class. Set up the coefficients
+ * with their right scaling factor as well ref: page 11 and 12 of datasheet
  */
-void MS5607::_read_calibration_coefficients()
-{
-    // cast these values as a float to prevent overflowing of the int after scaling
-   _c1 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C1)) * (1 << 16);
-   _c2 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C2)) * (1 << 17);
-   _c3 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C3)) * 7.8125E-3;
-   _c4 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C4)) * 1.5625E-2;
-   _c5 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C5)) * (1 << 8);
-   _c6 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C6)) * 1.192092896E-7;
-
+void MS5607::_read_calibration_coefficients() {
+    // cast these values as a float to prevent overflowing of the int after
+    // scaling
+    _c1 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C1)) * (1 << 16);
+    _c2 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C2)) * (1 << 17);
+    _c3 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C3)) * 7.8125E-3;
+    _c4 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C4)) * 1.5625E-2;
+    _c5 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C5)) * (1 << 8);
+    _c6 = static_cast<float>(_read_prom(MS5607_CMD_READ_PROM_C6)) * 1.192092896E-7;
 
     // read supplementary values to calculate CRC
     // float reserved_data = _read_prom(MS5607_CMD_READ_PROM_BASE);
     // float crc = _read_prom(MS5607_CMD_READ_PROM_CRC);
 
     // float coeffs[NUM_COEFFS + 2] = {
-    //     reserved_data, 
+    //     reserved_data,
     //     _c1, _c2, _c3,
-    //     _c4, _c5, _c6, 
+    //     _c4, _c5, _c6,
     //     crc
     // };
 
@@ -194,14 +195,14 @@ void MS5607::_read_calibration_coefficients()
 }
 
 /**
- * Validate CRC based on algorithm found here: https://www.amsys.de/downloads/notes/MS5XXX-C-code-example-for-MS56xx-MS57xx-MS58xx-AMSYS-an520e.pdf
- * 
+ * Validate CRC based on algorithm found here:
+ * https://www.amsys.de/downloads/notes/MS5XXX-C-code-example-for-MS56xx-MS57xx-MS58xx-AMSYS-an520e.pdf
+ *
  * TODO: this function has not been properly tested yet though
  */
-bool MS5607::_validate_crc4(uint16_t coeffs[NUM_COEFFS + 2]) 
-{
+bool MS5607::_validate_crc4(uint16_t coeffs[NUM_COEFFS + 2]) {
     uint16_t rem = 0;
-    
+
     uint16_t crc = coeffs[7] & 0xF;
     coeffs[7] &= crc & 0xFF00;
 
@@ -228,10 +229,9 @@ bool MS5607::_validate_crc4(uint16_t coeffs[NUM_COEFFS + 2])
 
 /**
  * Read the 24-bit ADC value
- * 
+ *
  */
-uint32_t MS5607::_read_adc() 
-{
+uint32_t MS5607::_read_adc() {
     digitalWrite(_CS_pin, LOW);
     _spi->beginTransaction(_spi_settings);
 
@@ -239,15 +239,14 @@ uint32_t MS5607::_read_adc()
     uint8_t first_byte = _spi->transfer(0);
     uint8_t second_byte = _spi->transfer(0);
     uint8_t third_byte = _spi->transfer(0);
-    
+
     _spi->endTransaction();
     digitalWrite(_CS_pin, HIGH);
 
     return (first_byte << 16 | second_byte << 8 | third_byte) & 0x00FFFFFF;
 }
 
-void MS5607::_setup_pressure_calculation()
-{
+void MS5607::_setup_pressure_calculation() {
     // these calibration values depend on dT and the last calculated temperature
     // return invalid value if dT and last calculated temperature do not yet exist
     if (_dT == -1 || _last_calculated_temperature == -1) {
@@ -259,12 +258,13 @@ void MS5607::_setup_pressure_calculation()
 }
 
 /**
- * test the SPI connection and expect a 0 value, since no conversion has been initiated yet
- * 
- * @note: assumes that the CS pin is already held low and that the SPI bus is already in a transaction
+ * test the SPI connection and expect a 0 value, since no conversion has been
+ * initiated yet
+ *
+ * @note: assumes that the CS pin is already held low and that the SPI bus is
+ * already in a transaction
  */
-bool MS5607::_test_spi()
-{
+bool MS5607::_test_spi() {
     uint32_t blank_adc_val = _read_adc();
     return blank_adc_val == 0;
 }
--- MS5607.h
+++ MS5607.h
@@ -5,21 +5,21 @@
 #include <SPI.h>
 
 /* MS5607 Command Set */
-#define MS5607_CMD_RESET              0x1E  /* Reset command */
+#define MS5607_CMD_RESET              0x1E /* Reset command */
 
 /* PROM Read Commands (Calibration data) */
-#define MS5607_CMD_READ_PROM_BASE     0xA0  /* Base address for PROM read */
-#define MS5607_CMD_READ_PROM_C1       0xA2  /* Pressure sensitivity */
-#define MS5607_CMD_READ_PROM_C2       0xA4  /* Pressure offset */
-#define MS5607_CMD_READ_PROM_C3       0xA6  /* Temperature coefficient of pressure sensitivity */
-#define MS5607_CMD_READ_PROM_C4       0xA8  /* Temperature coefficient of pressure offset */
-#define MS5607_CMD_READ_PROM_C5       0xAA  /* Reference temperature */
-#define MS5607_CMD_READ_PROM_C6       0xAC  /* Temperature coefficient of the temperature */
-#define MS5607_CMD_READ_PROM_CRC      0xAE  /* CRC check */
+#define MS5607_CMD_READ_PROM_BASE     0xA0 /* Base address for PROM read */
+#define MS5607_CMD_READ_PROM_C1       0xA2 /* Pressure sensitivity */
+#define MS5607_CMD_READ_PROM_C2       0xA4 /* Pressure offset */
+#define MS5607_CMD_READ_PROM_C3       0xA6 /* Temperature coefficient of pressure sensitivity */
+#define MS5607_CMD_READ_PROM_C4       0xA8 /* Temperature coefficient of pressure offset */
+#define MS5607_CMD_READ_PROM_C5       0xAA /* Reference temperature */
+#define MS5607_CMD_READ_PROM_C6       0xAC /* Temperature coefficient of the temperature */
+#define MS5607_CMD_READ_PROM_CRC      0xAE /* CRC check */
 #define NUM_COEFFS                    6
 
 /* ADC Read */
-#define MS5607_CMD_ADC_READ           0x00  /* Read ADC result */
+#define MS5607_CMD_ADC_READ           0x00 /* Read ADC result */
 
 /* Pressure Conversion Commands (D1) */
 #define MS5607_CMD_CONVERT_D1_OSR256  0x40
@@ -35,10 +35,8 @@
 #define MS5607_CMD_CONVERT_D2_OSR2048 0x56
 #define MS5607_CMD_CONVERT_D2_OSR4096 0x58
 
-
-class MS5607 
-{
-public:
+class MS5607 {
+  public:
     typedef enum OSR_Rate {
         OSR256,
         OSR512,
@@ -49,7 +47,7 @@ public:
 
     MS5607(SPIClass *spi_bus, uint8_t cs_pin, OSR_t osr_rate = OSR256);
 
-    bool initialize();
+    bool begin();
     void set_osr_rate(OSR_t osr_rate);
     uint32_t read_raw_temperature(bool &reading_is_valid);
     int32_t calculate_temperature(uint32_t raw_temperature);
@@ -58,29 +56,30 @@ public:
     float get_altitude(uint32_t t, uint32_t p);
     void dump_calibration_coeffs();
 
-private:
+  private:
     SPIClass *_spi;
     SPISettings _spi_settings;
 
     uint8_t _CS_pin;
 
     // calibration coeffs
-    float _c1 {-1};
-    float _c2 {-1};
-    float _c3 {-1};
-    float _c4 {-1};
-    float _c5 {-1};
-    float _c6 {-1};
+    float _c1{-1};
+    float _c2{-1};
+    float _c3{-1};
+    float _c4{-1};
+    float _c5{-1};
+    float _c6{-1};
 
     // OSR commands -- set based on the OSR rate
     uint16_t _pressure_command;
     uint16_t _temperature_command;
     long _adc_conversion_time_micro;
 
-    // used to record the last recorded temp and pressure, for calculation purposes
+    // used to record the last recorded temp and pressure, for calculation
+    // purposes
     float _last_calculated_temperature;
     float _dT; // diff between actual and reference temperature
-    
+
     // allow for pressure calculations with cached temperature readings
     float _last_calculated_offset;
     float _last_calculated_actual_sensitivity;
